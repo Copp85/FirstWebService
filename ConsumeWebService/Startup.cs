e9fe39966c7a48fe61c15619e42cf25dc64c5c64
Shane Coppinger <S00190494@mail.itsligo.ie>
2019-03-31 17:08:11 +0100

    ConsumeWebService

--- ConsumeWebService/Startup.cs
+++ ConsumeWebService/Startup.cs
@@ -2,18 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FirstWebService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace FirstWebService
+namespace ConsumeWebService
 {
     public class Startup
     {
@@ -28,11 +26,6 @@ namespace FirstWebService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-               services.AddDbContext<ToDoContext>(options =>
-                     options.UseInMemoryDatabase("mytempdb"));
-
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- ConsumeWebServices/Startup.cs
+++ ConsumeWebServices/Startup.cs
@@ -1,19 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FirstWebService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace FirstWebService
+namespace ConsumeWebServices
 {
     public class Startup
     {
@@ -27,12 +24,15 @@ namespace FirstWebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-               services.AddDbContext<ToDoContext>(options =>
-                     options.UseInMemoryDatabase("mytempdb"));
-
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,10 +44,14 @@ namespace FirstWebService
             }
             else
             {
+                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
             app.UseMvc();
         }
     }
